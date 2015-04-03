e36ebfd777c954b5f9f5b7abc79e8d1f7778a638
Chia <happy082807@gmail.com>
2015-04-03 22:46:12 +0800

    phonegap test(with php)

---
+++ pgtest/assets/www/main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<style type=text/css>
+	.user-info { height:50%;width:50%;float:left;}
+	.evaluate { height:50%;width:50%;float:right; }
+	.edge{ margin:1%;}
+	
+</style>
+<head class="page-header">
+	<meta charset="big-5" />
+    <title>My Web</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="css/star-rating.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="css/bootstrap-rating.min.css" rel="stylesheet" type="text/css" media="all">
+	<script src="js/star-rating.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap-rating.min.js" type="text/javascript"></script>
+	<script src="js/jquery-2.1.3.js" type="text/javascript"></script>
+</head>
+<body style="height:60%;margin:1%;">
+	<!--
+	class="img-responsive" 
+	<img src="icon.png"   height="100px">
+	<button class="btn dropdown-toggle" data-toggle="dropdown"> Magic <span class="caret"></span></button>
+	<img class="img-responsive" alt="Brand" src="icon.png" vertical-align="middle">
+	<p class="alert">Hello Web!</p>
+	-->
+	<h3>Hello world!<h3/>
+
+
+
+</body>
+
+
+</html>
